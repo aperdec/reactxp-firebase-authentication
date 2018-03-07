e1083ba51f40c2fab3eeb49405063d3c2b857261
Andrew Perdec <aperdec@gmail.com>
2018-03-07 15:37:49 -0700

    changed the export definition

--- index.android.js
+++ index.android.js
@@ -1,7 +1,9 @@
+'use strict';
+
 import * as auth from './src/react-common/auth';
 import * as firebase from './src/react-common/firebase';
 
-export {
+module.exports = {
     auth,
     firebase,
 };
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+'use strict';
+
 import * as auth from './src/web/auth';
 import * as firebase from './src/web/firebase';
 
-export {
+module.exports = {
     auth,
     firebase,
 };
